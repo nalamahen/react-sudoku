ded13f7c940ac406010095437dbc64ee7cde8660
Nallaiah Mahendramoorthy <nalamahen@nallaiahs-mbp.home>
2020-07-21 12:15:21 +0100

    add shuffle util method created by Fisher Yates.

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,4 +1,5 @@
-import { GRID } from 'typings';
+import { GRID, NUMBERS } from 'typings';
+import { shuffle } from 'utils';
 
 const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,8 +13,26 @@ const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ * A backtracking/recursive function to check all the possible combination of numbers until a solution is found.
+ * @param grid - 9x9 Sudoku Grid
+ */
 function fillGrid(grid: GRID) {
-  return grid;
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+
+    if (grid[row][col] === 0) {
+      // Do stuff
+      // recursive things
+      break;
+    }
+  }
 }
 
 export default fillGrid;
