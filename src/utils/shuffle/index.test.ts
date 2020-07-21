43e3031bf934f4b769a7ccc8ca5ef7a51435371c
Nallaiah Mahendramoorthy <nalamahen@nallaiahs-mbp.home>
2020-07-21 12:54:29 +0100

    add a unit test for for shuffle.

---
+++ src/utils/shuffle/index.test.ts
@@ -0,0 +1,18 @@
+import shuffle from './';
+
+describe('shuffle', () => {
+  it('returns an array with same length after being shuffle', () => {
+    const array = [1, 2, 3];
+    shuffle(array);
+    expect(array).toHaveLength(3);
+  });
+
+  it('returns and array with the same elements after being shuffled', () => {
+    const array = [1, 2, 3];
+    shuffle(array);
+    console.log('array:', array);
+    expect(array).toContain(1);
+    expect(array).toContain(1);
+    expect(array).toContain(1);
+  });
+});
